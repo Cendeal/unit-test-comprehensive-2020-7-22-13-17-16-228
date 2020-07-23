3563c2c7dc337af49bce0383842968550d9855c1
Cendeal <5205093You>
2020-07-23 22:23:56 +0800

    [guess number game][Cendeal]feature:implement guess when given 3421

--- src/main/java/game/GuessGame.java
+++ src/main/java/game/GuessGame.java
@@ -11,6 +11,16 @@ public class GuessGame {
         if (answer.equals(numbers)) {
             return "4A0B";
         }
+        String temp = this.answer;
+        for (String s : numbers.split("")) {
+            if (numbers.indexOf(s) == this.answer.indexOf(s)) {
+                break;
+            }
+            temp = temp.replace(s, "");
+        }
+        if (temp.length() == 0) {
+            return "0A4B";
+        }
         return "1A3B";
     }
 }
