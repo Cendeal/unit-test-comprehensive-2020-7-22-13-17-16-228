83f195608138f8565ab24256f022d153fc4f4956
Cendeal <5205093You>
2020-07-24 09:06:49 +0800

    [guess number game][Cendeal]feature:implement Validation when given 1222

--- src/main/java/game/Validation.java
+++ src/main/java/game/Validation.java
@@ -2,7 +2,16 @@ package game;
 
 public class Validation {
     private final int LENGTH = 4;
+
     public boolean isValid(String given) {
+        String[] numbers = given.split("");
+        for (String number : numbers) {
+            if (given.replaceAll(number, "").length() == LENGTH - 1) {
+                continue;
+            }
+            return false;
+
+        }
         return given.length() == LENGTH;
     }
 }
