2dc403cb6e7e7b821bbf5192b9aac34c1692cdd9
Cendeal <5205093You>
2020-07-24 08:33:37 +0800

    [guess number game][Cendeal]test:add should_return_true_when_is_valid_given_1234 test

---
+++ src/test/java/game/ValidationTestUnit.java
@@ -0,0 +1,18 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValidationTestUnit {
+    @Test
+    public void should_return_true_when_is_valid_given_1234() {
+        //given
+        String given = "1234";
+        //when
+        Validation validation = new Validation();
+        boolean actual = validation.isValid(given);
+        //then
+        assertTrue(actual);
+    }
+}
