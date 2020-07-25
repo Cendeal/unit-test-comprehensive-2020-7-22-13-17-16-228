03fa219089ada2fdf66ddf11f9c217a1df862a1b
Cendeal <1798062051@qq.com>
2020-07-25 19:30:22 +0800

    [guess number game][Cendeal]feature:implement answer generate

--- src/main/java/game/GuessAnswerGenerator.java
+++ src/main/java/game/GuessAnswerGenerator.java
@@ -1,9 +1,18 @@
 package game;
 
+import java.util.Random;
+
 public class GuessAnswerGenerator implements AnswerGenerator {
 
     @Override
     public String generate() {
-        return null;
+        StringBuilder builder = new StringBuilder("0123456789");
+        StringBuilder answer = new StringBuilder();
+        for(int i=0;i<4;i++){
+            int random = new Random().nextInt(builder.length());
+            answer.append(builder.charAt(random));
+            builder.deleteCharAt(random);
+        }
+        return answer.toString();
     }
 }
