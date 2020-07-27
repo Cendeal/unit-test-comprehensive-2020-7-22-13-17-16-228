9df7e3ad6291c9befb1c4aaa4d29332d389f1e90
Cendeal <1798062051@qq.com>
2020-07-27 22:44:33 +0800

    [guess number game][Cendeal]refactor:add  should_win_when_start_given_1234 test

--- src/test/java/game/GameApplicationTest.java
+++ src/test/java/game/GameApplicationTest.java
@@ -2,13 +2,28 @@ package game;
 
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class GameApplicationTest {
 
     @Test
-    void start() {
+    void should_win_when_start_given_1234() {
+        //given
+        InputHandler inputHandler = mock(InputHandler.class);
+        AnswerGenerator answerGenerator = mock(GuessAnswerGenerator.class);
+        given(answerGenerator.generate()).willReturn("1234");
+        GameApplication application = new GameApplication(inputHandler, answerGenerator);
+        //when
+        when(inputHandler.input()).thenReturn("1234");
+        when(inputHandler.command()).thenReturn("N");
+        application.start();
+
+        //then
+        inputHandler.close();
+
+
     }
 
-    @Test
-    void main() {
-    }
 }
